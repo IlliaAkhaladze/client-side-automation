a4b4bf424d3ad25bcd68b21168897290d10016fe
illia-akh <illia.akhaladze@gmail.com>
2023-09-26 09:52:53 +0300

    Update definiton

--- Tests/Frontend/Selenium.Automation.Tests/Features/Header.Definition.cs
+++ Tests/Frontend/Selenium.Automation.Tests/Features/Header.Definition.cs
@@ -1,49 +1,48 @@
-using System;
-using TechTalk.SpecFlow;
 using FluentAssertions;
 
-using Selenium.Automation.Model.Domain.Login;
 using Selenium.Automation.Model.Domain.Header;
+using Selenium.Automation.Model.Domain.Login;
 using Selenium.Automation.Model.Domain.Navigation;
 
+using TechTalk.SpecFlow;
 
-
-namespace Selenium.Automation.Tests.Features;
-
-[Binding, Scope(Feature = "Header")]
-public class HeaderDefinition
+namespace Selenium.Automation.Tests.Features
 {
-    private readonly INavigationSteps _navigationSteps;
-    private readonly ILoginSteps _loginSteps;
-    private readonly IHeaderSteps _headerSteps;
+	[Binding, Scope(Feature = "Header")]
+	public class HeaderDefinition
+	{
+		private readonly INavigationSteps _navigationSteps;
+		private readonly ILoginSteps _loginSteps;
+		private readonly IHeaderSteps _headerSteps;
 
-    public HeaderDefinition(
-        INavigationSteps navigationSteps,
-        ILoginSteps loginSteps,
-        IHeaderSteps headerSteps)
-    {
-        _navigationSteps = navigationSteps;
-        _loginSteps = loginSteps;
-        _headerSteps = headerSteps;
-    }
+		public HeaderDefinition(
+			INavigationSteps navigationSteps,
+			ILoginSteps loginSteps,
+			IHeaderSteps headerSteps)
+		{
+			_navigationSteps = navigationSteps;
+			_loginSteps = loginSteps;
+			_headerSteps = headerSteps;
+		}
 
-    [Given(@"I open main view")]
-    public void GivenIOpenMainView()
-    {
-        _loginSteps.OpenMainView();
-    }
+		[Given(@"I open main view")]
+		public void GivenIOpenMainView()
+		{
+			_loginSteps.OpenMainView();
+		}
 
-    [When(@"I change language to UA")]
-    public void WhenIChangeLanguageToUA()
-    {
-        _headerSteps.ChangeLanguageUA();
-    }
+		[When(@"I change language to UA")]
+		public void WhenIChangeLanguageToUA()
+		{
+			_headerSteps.ChangeLanguageUA();
+		}
 
-    [Then(@"I see language is '([^']*)'")]
-    public void ThenISeeLanguageIs(string p0)
-    {
-         bool actualValue = _headerSteps.IsLanguageUA();
-         actualValue.Should().BeTrue();
-    }
+		[Then(@"I see language is '([^']*)'")]
+		public void ThenISeeLanguageIs(string p0)
+		{
+			bool actualValue = _headerSteps.IsLanguageUA();
+			actualValue.Should().BeTrue();
+		}
 
+	}
 }
